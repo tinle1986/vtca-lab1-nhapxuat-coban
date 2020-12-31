79f5a194c839f012a4e3c4db54fafdacdb1ba03c
tinle1986 <letrungtin1986@gmail.com>
2020-12-31 18:23:35 +0700

    first commit

---
+++ src/com/ttlecom/Main.java
@@ -0,0 +1,130 @@
+package com.ttlecom;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// write your code here
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("STT\tHo ten\t\t\tN.sinh\tG.tinh\tC.cao\tC.nang\tQue quan\t\t\tC.nganh\t\tDTB");
+		System.out.println("================================================================================================");
+		System.out.print("1\t");
+		System.out.print("Nguyen Van A\t");
+		System.out.print("1998\t");
+		System.out.print("F\t\t");
+		System.out.print("1.64\t");
+		System.out.print("85.3\t");
+		System.out.print("Ho Chi Minh\t\t\t");
+		System.out.print("CNTT\t\t");
+		System.out.println("8.5");
+
+		System.out.print("1\t");
+		System.out.print("Nguyen Van A\t");
+		System.out.print("1998\t");
+		System.out.print("F\t\t");
+		System.out.print("1.64\t");
+		System.out.print("85.3\t");
+		System.out.print("Ho Chi Minh\t\t\t");
+		System.out.print("CNTT\t\t");
+		System.out.println("8.5");
+
+		System.out.print("1\t");
+		System.out.print("Nguyen Van A\t");
+		System.out.print("1998\t");
+		System.out.print("F\t\t");
+		System.out.print("1.64\t");
+		System.out.print("85.3\t");
+		System.out.print("Ho Chi Minh\t\t\t");
+		System.out.print("CNTT\t\t");
+		System.out.println("8.5");
+
+		System.out.println("***Moi nhap ung vien so 1:");
+		System.out.println("\t+Nhap ho ten:");
+		String ten1 = scanner.nextLine();
+		System.out.println("\t+Nhap nam sinh:");
+		int nam1 = Integer.parseInt(scanner.nextLine());
+		System.out.println("\t+Nhap gioi tinh:");
+		String gioiTinh1 = scanner.nextLine();
+		System.out.println("\t+Nhap chieu cao:");
+		float chieuCao1 = Float.parseFloat(scanner.nextLine());
+		System.out.println("\t+Nhap can nang:");
+		int canNang1 = Integer.parseInt(scanner.nextLine());
+		System.out.println("\t+Nhap que quan:");
+		String queQuan1 = scanner.nextLine();
+		System.out.println("\t+Nhap chuyen nganh:");
+		String chuyenNganh1 = scanner.nextLine();
+		System.out.println("\t+Nhap diem TB:");
+		float dtb1 = Float.parseFloat(scanner.nextLine());
+
+		System.out.println("***Moi nhap ung vien so 2:");
+		System.out.println("\t+Nhap ho ten:");
+		String ten2 = scanner.nextLine();
+		System.out.println("\t+Nhap nam sinh:");
+		int nam2 = Integer.parseInt(scanner.nextLine());
+		System.out.println("\t+Nhap gioi tinh:");
+		String gioiTinh2 = scanner.nextLine();
+		System.out.println("\t+Nhap chieu cao:");
+		float chieuCao2 = Float.parseFloat(scanner.nextLine());
+		System.out.println("\t+Nhap can nang:");
+		int canNang2 = Integer.parseInt(scanner.nextLine());
+		System.out.println("\t+Nhap que quan:");
+		String queQuan2 = scanner.nextLine();
+		System.out.println("\t+Nhap chuyen nganh:");
+		String chuyenNganh2 = scanner.nextLine();
+		System.out.println("\t+Nhap diem TB:");
+		float dtb2 = Float.parseFloat(scanner.nextLine());
+
+		System.out.println("***Moi nhap ung vien so 3:");
+		System.out.println("\t+Nhap ho ten:");
+		String ten3 = scanner.nextLine();
+		System.out.println("\t+Nhap nam sinh:");
+		int nam3 = Integer.parseInt(scanner.nextLine());
+		System.out.println("\t+Nhap gioi tinh:");
+		String gioiTinh3 = scanner.nextLine();
+		System.out.println("\t+Nhap chieu cao:");
+		float chieuCao3 = Float.parseFloat(scanner.nextLine());
+		System.out.println("\t+Nhap can nang:");
+		int canNang3 = Integer.parseInt(scanner.nextLine());
+		System.out.println("\t+Nhap que quan:");
+		String queQuan3 = scanner.nextLine();
+		System.out.println("\t+Nhap chuyen nganh:");
+		String chuyenNganh3 = scanner.nextLine();
+		System.out.println("\t+Nhap diem TB:");
+		float dtb3 = Float.parseFloat(scanner.nextLine());
+
+		System.out.println("========================================");
+		System.out.println("Danh sach cac ung vien da nhap");
+		System.out.println("========================================");
+		System.out.println("***Ung vien so 1:");
+		System.out.println("+Ho ten: " + ten1);
+		System.out.println("+Nam sinh: " + nam1);
+		System.out.println("+Gioi tinh: " + gioiTinh1);
+		System.out.println("+Chieu cao: " + chieuCao1);
+		System.out.println("+Can nang: " + canNang1);
+		System.out.println("+Que quan: " + queQuan1);
+		System.out.println("+Chuyen nganh: " + chuyenNganh1);
+		System.out.println("+Diem TB: " + dtb1);
+
+		System.out.println("***Ung vien so 2:");
+		System.out.println("+Ho ten: " + ten2);
+		System.out.println("+Nam sinh: " + nam2);
+		System.out.println("+Gioi tinh: " + gioiTinh2);
+		System.out.println("+Chieu cao: " + chieuCao2);
+		System.out.println("+Can nang: " + canNang2);
+		System.out.println("+Que quan: " + queQuan2);
+		System.out.println("+Chuyen nganh: " + chuyenNganh2);
+		System.out.println("+Diem TB: " + dtb2);
+
+		System.out.println("***Ung vien so 3:");
+		System.out.println("+Ho ten: " + ten3);
+		System.out.println("+Nam sinh: " + nam3);
+		System.out.println("+Gioi tinh: " + gioiTinh3);
+		System.out.println("+Chieu cao: " + chieuCao3);
+		System.out.println("+Can nang: " + canNang3);
+		System.out.println("+Que quan: " + queQuan3);
+		System.out.println("+Chuyen nganh: " + chuyenNganh3);
+		System.out.println("+Diem TB: " + dtb3);
+	}
+}
